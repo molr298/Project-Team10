ddc0f80a23cf2af6b77abc5ca90832c9ca274053
letri-hub <61229133+letri-hub@users.noreply.github.com>
2020-08-02 21:49:17 +0700

    Delete some unsued lines

--- Project-Team10/Project-Team10/Admin.cpp
+++ Project-Team10/Project-Team10/Admin.cpp
@@ -30,11 +30,6 @@ void Admin::saveUser()
 {
 	remove("C:/Users/MyPC/source/repos/Project-Team10/Project-Team10/Data/User.txt");
 	ofstream fout("C:/Users/MyPC/source/repos/Project-Team10/Project-Team10/Data/User.txt");
-	/*if (!fout.is_open()) {
-		cout << "Can't write user information!!" << endl;
-		return;
-	}*/
-
 	fout << listUser.size() << endl;
 	for (int i = 0; i < listUser.size(); i++)
 		listUser[i].saveAnAccountInfor(fout);
